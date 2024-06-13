9199a9854793e2ba1b6f3ad2720caad376ff36d1
pcrespan <pedrocrespan@hotmail.com>
2024-06-13 12:20:21 -0300

    Changing authorization of DoctorController

--- backend/src/main/java/com/ras/baymax/controllers/DoctorController.java
+++ backend/src/main/java/com/ras/baymax/controllers/DoctorController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/doctors")
 @CrossOrigin
-@PreAuthorize("hasRole('ROLE_DOCTOR')")
+@PreAuthorize("hasRole('ROLE_DOCTOR') or hasRole('ROLE_NURSE')")
 public class DoctorController {
 
     @Autowired
